bdaf730c3165238e7be2e0229f327476a52a89cf
sarangvs <sarang6180@gmail.com>
2021-12-06 16:45:58 +0530

    first commit

---
+++ lib/Views/login_screen.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: SizedBox(
+            height: screenHeight,
+            width: screenWidth,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Image.asset('Assets/loginimage.png'),
+                Container(
+                  // color: Colors.red,
+                  width: screenWidth * 0.76,
+                  margin: EdgeInsets.only(left: screenWidth * 0.12),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Login',
+                        textAlign: TextAlign.start,
+                        style: GoogleFonts.k2d(
+                            textStyle: const TextStyle(fontSize: 27),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.02,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            hintText: 'Email ID or Phone',
+                            hintStyle: GoogleFonts.k2d(fontSize: 15),
+                            prefixIcon: const Icon(
+                              Icons.alternate_email_sharp,
+                              size: 20,
+                              color: Colors.grey,
+                            ),
+                            focusedBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.grey, width: 1.0))),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.01,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Password',
+                          hintStyle: GoogleFonts.k2d(fontSize: 15),
+                          prefixIcon: const Icon(
+                            Icons.lock,
+                            size: 20,
+                            color: Colors.grey,
+                          ),
+                          focusedBorder: const UnderlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 1.0)),
+                        ),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.05,
+                      ),
+                      Container(
+                        height: screenHeight * 0.06,
+                        decoration: const BoxDecoration(
+                            color: Colors.black,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(12))),
+                        child: Center(
+                          child: Text(
+                            'Login',
+                            style: GoogleFonts.k2d(
+                                color: Colors.white, fontSize: 18),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                Container(
+                  width: screenWidth * 0.76,
+                  margin: EdgeInsets.only(left: screenWidth * 0.12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Or Login with...',
+                        style: GoogleFonts.k2d(color: Colors.grey[600]),
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.01,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                                border:
+                                    Border.all(width: 0.9, color: Colors.grey)),
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Image.asset('Assets/google.png'),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: Colors.white,
+                                border:
+                                    Border.all(width: 0.9, color: Colors.grey)),
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Image.asset('Assets/facebook.png'),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: screenHeight * 0.02,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'New to App?',
+                            style: GoogleFonts.k2d(color: Colors.grey),
+                          ),
+                          Text(
+                            'Register',
+                            style: GoogleFonts.k2d(fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:social_media/Views/login_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Social Media',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:  const LoginScreen(),
+    );
+  }
+}
+
